ff5b2cad82ce1cc1f2148d6a843b1bf1becbbb2e
Luisferrer02 <luisferrer2002@gmail.com>
2025-03-13 10:29:36 +0100

    middleware

--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,17 @@ const api = axios.create({
   baseURL: 'https://backend-restaurantes-wq8c.onrender.com/api', // Cambia esto al URL de tu backend en el futuro
 });
 
+// Interceptor para agregar el token a cada solicitud
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+
 export default api;
